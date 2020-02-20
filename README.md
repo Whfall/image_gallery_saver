6e455e99980b6a701b78a2a5b58f78b1c1986b40
Egor Sidorov <egorsekb@gmail.com>
2020-02-21 02:17:21 +0300

    New

---
+++ README.md
@@ -0,0 +1,40 @@
+# image_gallery_saver
+
+[![Build Status](https://travis-ci.org/hui-z/image_gallery_saver.svg?branch=master)](https://travis-ci.org/hui-z/image_gallery_saver#)
+[![pub package](https://img.shields.io/pub/v/image_gallery_saver.svg)](https://pub.dartlang.org/packages/image_gallery_saver)
+[![license](https://img.shields.io/github/license/mashape/apistatus.svg)](https://choosealicense.com/licenses/mit/)
+
+We use image_picker to selecte images from the Android and iOS image library, but it can't save image to gallery，this plugin can provide this feature.
+
+## Usage
+
+To use this plugin, add image_gallery_saver as a dependency in your pubspec.yaml file.
+
+## iOS
+Your project need create with swift
+Add the following keys to your Info.plist file, located in <project root>/ios/Runner/Info.plist:
+ * NSPhotoLibraryAddUsageDescription - describe why your app needs permission for the photo library. This is called Privacy - Photo Library Additions Usage Description in the visual editor
+ 
+ ##  Android
+ need to ask for storage permission to save the image. You can using https://github.com/BaseflowIT/flutter-permission-handler to handle permission.
+
+## Example
+save net image
+``` dart
+_save() async {
+    var response = await Dio().get("https://ss0.baidu.com/94o3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=a62e824376d98d1069d40a31113eb807/838ba61ea8d3fd1fc9c7b6853a4e251f94ca5f46.jpg", options: Options(responseType: ResponseType.bytes));
+    final result = await ImageGallerySaver.saveImage(Uint8List.fromList(response.data));
+    print(result);
+  }
+```
+
+save net video
+``` dart
+_saveVideo() async {
+    var appDocDir = await getTemporaryDirectory();
+    String savePath = appDocDir.path + "/temp.mp4";
+    await Dio().download("http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4", savePath);
+    final result = await ImageGallerySaver.saveFile(savePath);
+    print(result);
+ }
+```
---
+++ android/src/main/kotlin/com/example/imagegallerysaver/ImageGallerySaverPlugin.kt
@@ -0,0 +1,111 @@
+package com.example.imagegallerysaver
+
+import android.content.Intent
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Environment
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.PluginRegistry.Registrar
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+
+class ImageGallerySaverPlugin(private val registrar: Registrar): MethodCallHandler {
+
+  companion object {
+    @JvmStatic
+    fun registerWith(registrar: Registrar) {
+      val channel = MethodChannel(registrar.messenger(), "image_gallery_saver")
+      channel.setMethodCallHandler(ImageGallerySaverPlugin(registrar))
+    }
+  }
+
+  override fun onMethodCall(call: MethodCall, result: Result): Unit {
+    when {
+        call.method == "saveImageToGallery" -> {
+          val image = call.arguments as ByteArray
+          result.success(saveImageToGallery(BitmapFactory.decodeByteArray(image,0,image.size)))
+        }
+        call.method == "saveFileToGallery" -> {
+          val map = call.arguments as HashMap<*,*>
+          val path = map["file"] as String
+          val name = map["fileName"] as String
+          val album = map["albumName"] as String
+          result.success(saveFileToGallery(path, name, album))
+        }
+        else -> result.notImplemented()
+    }
+
+  }
+
+   private fun generateFile(extension: String = "", name: String = "", album: String = ""): File {
+    val storePath =  Environment.getExternalStorageDirectory().absolutePath + File.separator + album
+    val appDir = File(storePath)
+    if (!appDir.exists()) {
+      appDir.mkdir()
+    }
+    var fileName = name
+    if (extension.isNotEmpty()) {
+      fileName += ("." + extension)
+    }
+    return File(appDir, fileName)
+  }
+
+  private fun saveImageToGallery(bmp: Bitmap): String {
+    val context = registrar.activeContext().applicationContext
+    val file = generateFile("png","test")
+    try {
+      val fos = FileOutputStream(file)
+      bmp.compress(Bitmap.CompressFormat.PNG, 60, fos)
+      fos.flush()
+      fos.close()
+      val uri = Uri.fromFile(file)
+      context.sendBroadcast(Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, uri))
+      return uri.toString()
+    } catch (e: IOException) {
+      e.printStackTrace()
+    }
+    return ""
+  }
+
+  private fun saveFileToGallery(filePath: String, fileName: String, albumName: String): String {
+    val context = registrar.activeContext().applicationContext
+    return try {
+      val originalFile = File(filePath)
+      val file = generateFile(originalFile.extension, fileName, albumName)
+      originalFile.copyTo(file, true)
+
+      val uri = Uri.fromFile(file)
+      context.sendBroadcast(Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, uri))
+      return uri.toString()
+    } catch (e: IOException) {
+      e.printStackTrace()
+      ""
+    }
+  }
+
+  private fun getApplicationName(): String {
+    val context = registrar.activeContext().applicationContext
+    var ai: ApplicationInfo? = null
+    try {
+        ai = context.packageManager.getApplicationInfo(context.packageName, 0)
+    } catch (e: PackageManager.NameNotFoundException) {
+    }
+    var appName: String
+    appName = if (ai != null) {
+      val charSequence = context.packageManager.getApplicationLabel(ai)
+      StringBuilder(charSequence.length).append(charSequence).toString()
+    } else {
+      "image_gallery_saver"
+    }
+    return  appName
+  }
+
+
+}
